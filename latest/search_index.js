4ef5c60f99112e005fab4b9386ea632f53df2414
autodocs <autodocs>
2018-10-05 20:55:33 +0000

    build based on 7826d52

--- latest/search_index.js
+++ latest/search_index.js
@@ -77,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Names",
     "title": "names",
     "category": "section",
-    "text": "DoubleFloats is the package.  Double64 and Double32 are the exported types.Double64 is a pair of Float64s, used to provide 106 bit significands with 11 bit exponents.Double32 is a pair of Float32s, used to provide 48 bit significands with 8 bit exponents."
+    "text": "DoubleFloats is the package.  Double64 and Double32 are the exported types.Double64s hold a pair of Float64s, used to provide 106 bit significands with 11 bit exponents.Double32s hold a pair of Float32s, used to provide 48 bit significands with 8 bit exponents."
 },
 
 {
